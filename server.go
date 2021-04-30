4bf7042b1c03c20c5216ac5057dc51a04bb24600
Andreas Motl <andreas.motl@crate.io>
2021-04-30 17:04:31 +0200

    Prepare next development iteration

--- server.go
+++ server.go
@@ -25,7 +25,7 @@ import (
 	yaml "gopkg.in/yaml.v2"
 )
 
-const version = "0.3.0"
+const version = "0.4.0-dev"
 
 var (
 	listenAddress = flag.String("web.listen-address", ":9268", "Address to listen on for Prometheus requests.")
