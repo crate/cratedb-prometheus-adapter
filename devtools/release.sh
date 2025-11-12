110cf8530ece0aabd4eb7644a20be0afdcf21e10
Andreas Motl <andreas.motl@crate.io>
2025-11-12 18:29:07 +0100

    Release: Fix release tooling that prevented production of 0.5.10
    `git tag` does not output tags in natural ordering, so `tail -n1` will
    not do the right thing in displaying the most recent version.

--- devtools/release.sh
+++ devtools/release.sh
@@ -12,7 +12,7 @@ NAME="cratedb-prometheus-adapter"
 
 # Use most recent git tag as version number.
 # TODO: Should this be made more elaborate, to also account for patch and nightly releases?
-VERSION="$(git tag | tail -n1)"
+VERSION="$(git tag --sort version:refname | tail -n1)"
 
 # Build program for multiple architectures.
 docker build --file=devtools/release.Dockerfile --tag=build-cpa-all --build-arg="NAME=${NAME}" --build-arg="VERSION=${VERSION}" --progress=plain .
